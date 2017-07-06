7bcfc36ef762e6d741a94570109d4e19eb5354b1
Aaron Shaneen <aaron.shaneen@gmail.com>
2017-07-06 12:14:59 -0600

    top bar complete but not final

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,15 +1,26 @@
 <template>
-  <nav class="navbar navbar-default">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <a class="navbar-brand" href="#">Weber Classifieds</a>
-      </div>
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Dashboard</a></li>
-        <li><a href="#">Posts</a></li>
-        <li><a href="#">Messages</a></li>
-        <li><a href="#">Profile</a></li>
+  <nav class="navbar navbar-toggleable-md" style="background-color: rgb(73, 38, 101);">
+    <button class="navbar-toggler navbar-toggler-right"  style="color:white" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" style="color:white" href="#">Weber Classifieds</a>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Profile</a>
+        </li>
       </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="text" placeholder="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" style="color:white" type="submit">Search</button>
+      </form>
     </div>
   </nav>
 </template>
