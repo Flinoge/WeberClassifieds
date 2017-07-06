bf772a2a324af2dfec89dd20a5a098254e21cc02
Richard Dawson <bobafett1374@gmail.com>
2017-07-05 18:59:49 -0600

    started topbar

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,6 +1,9 @@
 <template>
-  <navbar>
-    Nav Bar
+  <navbar style="background-color: #808080; height: 50px;">
+    <span style="font-size: 30px; color: white;">&nbsp;&nbsp;&nbsp;Weber State Classifieds</span>
+    <span style="color: white; font-size: 20px; margin-left: 25px;"><a style="color: white;" href="/#/dashboard">Dashboard</a></span>
+    <span style="float: right; color: white; font-size: 20px; margin-right: 10px;"><a style="color: white; margin-top: 15px;" href="/#/editprofile">Profile</a></span>
+    <span></span>
   </navbar>
 </template>
 
@@ -9,35 +12,3 @@
     name: 'main'
   }
 </script>
-
-<!--<template>
-  <nav class="navbar navbar-default">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <a class="navbar-brand" href="#">Weber Classifieds</a>
-      </div>
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Classifieds</a></li>
-        <li><a href="#">Posts</a></li>
-        <li><a href="#">Messages</a></li>
-        <li><a href="#">Profile</a></li>
-      </ul>
-      <form class="navbar-form navbar-left">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
-      </ul>
-    </div>
-  </nav>
-</template>
-
-<script>
-  export default {
-    name: 'main'
-  }
-</script>-->
