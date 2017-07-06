9bdff94dfa0cbb18d663fd424cf0c4334549c7e6
Aaron Shaneen <aaron.shaneen@gmail.com>
2017-07-05 20:34:50 -0600

    pushing top bar

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,36 +1,14 @@
 <template>
-  <navbar>
-    Nav Bar
-  </navbar>
-</template>
-
-<script>
-  export default {
-    name: 'main'
-  }
-</script>
-
-<!--<template>
   <nav class="navbar navbar-default">
     <div class="container-fluid">
       <div class="navbar-header">
         <a class="navbar-brand" href="#">Weber Classifieds</a>
       </div>
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Classifieds</a></li>
+        <li class="active"><a href="#">Dashboard</a></li>
         <li><a href="#">Posts</a></li>
         <li><a href="#">Messages</a></li>
         <li><a href="#">Profile</a></li>
-      </ul>
-      <form class="navbar-form navbar-left">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
       </ul>
     </div>
   </nav>
@@ -40,4 +18,4 @@
   export default {
     name: 'main'
   }
-</script>-->
+</script>
